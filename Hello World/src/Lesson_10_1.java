bd651b5ab7cbec2b4cc3e6bedab40a065ccb98aa
DearCuba <rovbo85@gmail.com>
2023-09-27 09:38:39 +0500

    Complete lesson 10_1 and add lesson 10_2.

--- Hello World/src/Lesson_10_1.java
+++ Hello World/src/Lesson_10_1.java
@@ -7,36 +7,35 @@ import java.util.Scanner;
  * (+) 4) Упростится ли использование сканера внутри методов чтения с клавиатуры?
  */
 
-public class Lesson_10_1_Rectangle {
+public class Lesson_10_1 {
 
   // Поля класа:
   public static String vertical = "|";
   public static String horizontal = "=";
-  public static String corner = ".";
+  public static String corner = "*";
   public static String space = " ";
 
   public static void main(String[] args) {
 
-    // Вводим значения длины и ширины прямоугольника
+// Вводим значения длины и ширины прямоугольника
     System.out.print("Input length of rectangle: ");
     int length = scanner();
     System.out.print("Input width of rectangle: ");
     int width = scanner();
 
-    // Выводим прямоугольник в консоль используя методы
+// Выводим прямоугольник в консоль используя методы
     stick(corner, width);
     body(length, width);
     stick(corner, width);
   }
 
-  // Методы:
-
+// Методы:
   public static int scanner() {
     Scanner scanner = new Scanner(System.in);
     return scanner.nextInt();
   }
 
-  // Выводим в консоль верхнюю и нижнюю часть прямоугольника
+// Выводим в консоль верхнюю и нижнюю часть прямоугольника
   static void stick(String corner, int width) {
     System.out.print(corner);
     for(int a = 1; a <= width; a++) {
